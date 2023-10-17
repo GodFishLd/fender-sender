95b774d2c58fffee019f824fa75a7b7b992e39d7
GodFishLd <147598375+GodFishLd@users.noreply.github.com>
2023-10-18 01:00:16 +0700

    Update home.js

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -9,9 +9,6 @@ try {
 // Cloaking Code
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     const popup = open("about:blank", "_blank")
-    if (!popup || popup.closed) {
-        alert("Please allow popups and redirects.")
-    } else {
         const doc = popup.document
         const iframe = doc.createElement("iframe")
         const style = iframe.style
